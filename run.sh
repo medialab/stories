228dcfb7e32f1e959dc7e0463e9c5b64112fc36d
bmaz <beatrice.mazoyer@gmail.com>
2023-02-08 17:19:35 +0100

    Update run.sh since date is needed in eval

--- run.sh
+++ run.sh
@@ -16,20 +16,21 @@ TOTAL=`xsv count $1`
 
 echo "1. Extracting vocabulary"
 echo "------------------------"
-$TWEMBEDDINGS vocab $1 --total $TOTAL --tsv > $TMP_FOLDER/vocab.csv
+$TWEMBEDDINGS vocab $1 --total $TOTAL > $TMP_FOLDER/vocab.csv
 echo
 
 echo "2. Determining window size"
 echo "--------------------------"
-WINDOW=`$TWEMBEDDINGS window $1 --raw --total $TOTAL --tsv`
+WINDOW=`$TWEMBEDDINGS window $1 --raw --total $TOTAL`
 echo "Optimal window size should be: $WINDOW"
 echo
 
 echo "3. Applying clustering algorithm"
 echo "--------------------------------"
-$TWEMBEDDINGS nn $TMP_FOLDER/vocab.csv $1 -w $WINDOW --total $TOTAL --tsv --threshold 0.6 > $TMP_FOLDER/nn.csv
+$TWEMBEDDINGS nn $TMP_FOLDER/vocab.csv $1 -w $WINDOW --total $TOTAL --threshold 0.7 > $TMP_FOLDER/nn.csv
 echo
 
 echo "4. Evaluating"
 echo "-------------"
-$TWEMBEDDINGS eval $2 $TMP_FOLDER/nn.csv --total $TOTAL
+xsv join --left id $1 id $TMP_FOLDER/nn.csv | xsv select id,created_at,nearest_neighbor,thread_id,distance > $TMP_FOLDER/nn_dated.csv
+$TWEMBEDDINGS eval $2 $TMP_FOLDER/nn_dated.csv --total $TOTAL --datecol created_at
