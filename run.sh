69b4df25b2d2e77fcce10fcbd4a3561287a0baea
bmaz <beatrice.mazoyer@gmail.com>
2023-02-09 16:42:52 +0100

    Change twembeddings to singerie

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-TMP_FOLDER=./.twembeddings
+TMP_FOLDER=./.singerie
 
 # Cleanup
 rm -rf $TMP_FOLDER
@@ -11,26 +11,26 @@ mkdir $TMP_FOLDER
 cargo build --release
 echo
 
-TWEMBEDDINGS="./target/release/twembeddings"
+SINGERIE="./target/release/singerie"
 TOTAL=`xsv count $1`
 
 echo "1. Extracting vocabulary"
 echo "------------------------"
-$TWEMBEDDINGS vocab $1 --total $TOTAL > $TMP_FOLDER/vocab.csv
+$SINGERIE vocab $1 --total $TOTAL > $TMP_FOLDER/vocab.csv
 echo
 
 echo "2. Determining window size"
 echo "--------------------------"
-WINDOW=`$TWEMBEDDINGS window $1 --raw --total $TOTAL`
+WINDOW=`$SINGERIE window $1 --raw --total $TOTAL`
 echo "Optimal window size should be: $WINDOW"
 echo
 
 echo "3. Applying clustering algorithm"
 echo "--------------------------------"
-$TWEMBEDDINGS nn $TMP_FOLDER/vocab.csv $1 -w $WINDOW --total $TOTAL --threshold 0.7 > $TMP_FOLDER/nn.csv
+$SINGERIE nn $TMP_FOLDER/vocab.csv $1 -w $WINDOW --total $TOTAL --threshold 0.7 > $TMP_FOLDER/nn.csv
 echo
 
 echo "4. Evaluating"
 echo "-------------"
 xsv join --left id $1 id $TMP_FOLDER/nn.csv | xsv select id,created_at,nearest_neighbor,thread_id,distance > $TMP_FOLDER/nn_dated.csv
-$TWEMBEDDINGS eval $2 $TMP_FOLDER/nn_dated.csv --total $TOTAL --datecol created_at
+$SINGERIE eval $2 $TMP_FOLDER/nn_dated.csv --total $TOTAL --datecol created_at
